267e8bab3fe683026f33a7286febdb3d92fac0bf
fady6 <65589663+fady6@users.noreply.github.com>
2020-11-29 23:27:51 +0200

    Update Arduino_final_project.ino

--- Arduino_final_project/Arduino_final_project.ino
+++ Arduino_final_project/Arduino_final_project.ino
@@ -1,22 +1,38 @@
 // SERVO MOTOR
 #include <Servo.h>
 Servo myservo;
-int pos = 0;
+
+
+
 // COMPONENTS
 int buzzer = 8;
 int leftLDR = A0;
 int rightLDR = A1;
 int motor = 6;
 int button = 12;
+
+
+
 // READING
 int btnRead;
+
+
+
 // VARIABLES
 int peopleCount = 0;
 int enter;
 int out;
 int flag = false;
+int pos = 0;
+
+
+
 //Functions
 void entrySystem ();
+void exitSystem ();
+
+
+
 void setup()
 {
   pinMode(buzzer, OUTPUT);
@@ -26,6 +42,8 @@ void setup()
   myservo.attach(6);
   Serial.begin(9600);
 }
+
+
 
 void loop()
 {
@@ -43,38 +61,45 @@ void loop()
     }
   }
 }
-  void entrySystem (){ 
-     if (peopleCount <5){
-      enter = analogRead (leftLDR);
-      if (enter<=55){
-        peopleCount ++;
-        Serial.print(peopleCount);
-        Serial.println();
+
+
+
+
+
+
+
+void entrySystem (){ 
+  if (peopleCount <5){
+    enter = analogRead (leftLDR);
+    if (enter<=55){
+      peopleCount ++;
+      Serial.print(peopleCount);
+      Serial.println();
       
 // START SERVO MOTOR MOTION
       for (pos = 0; pos <= 180; pos += 1) { 
         myservo.write(pos);              
-        delay(5);                     
-  }  
-  }  
+        delay(50);}  
+  	  }  
      }
    
-    // START BUZZER SOUND
-    else 
-    {
+// START BUZZER SOUND
+    else
+      for (int i=1; i<=3; i++){
       tone(buzzer, 1000);
-      delay(1000);
-      noTone(buzzer);
-    }
-   
-  }
+      delay(500);
+      noTone(buzzer);}
+ }
 
-  void exitSystem (){
+
+
+
+
+void exitSystem (){
 // START ENTRY CONDITION OF PEOPLE
-    if (peopleCount >=1){
-      out = analogRead (rightLDR);
+  if (peopleCount >=1){
+    out = analogRead (rightLDR);
       if (out <=55){
-  
         peopleCount -- ;
         Serial.print(peopleCount);
         Serial.println();
@@ -82,9 +107,7 @@ void loop()
 // START SERVO MOTOR MOTION
     for (pos = 180; pos >= 0; pos +=-1) { 
       myservo.write(pos);              
-      delay(5);
-                             
+      delay(50);}
     }
   }
-}
-  }                      
+ }                      
